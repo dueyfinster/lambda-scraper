359b62b02f6d73fab7083f158e15141eaaba1d8c
Neil Grogan <neil@grogan.ie>
2017-08-22 10:24:39 +0100

    Updated slack.js

--- src/slack.js
+++ src/slack.js
@@ -1,1 +1,28 @@
+import Slack from 'node-slack';
+import config from '../config';
 
+export default class Slacker {
+  
+  async run(message) {
+    var slack = new Slack(config.url);
+    return slack.send({
+          text: "Today's Menu:",
+          username: message.title,
+          attachments: [
+            {
+              "fallback": message.soup1 + ' & ' + message.soup2,
+              "title": "Soups",
+              "text": message.soup1 + '\n' + message.soup2,
+              "color": "#00bcd4"
+            },
+            {
+              "fallback": message.main1 + '\n' + message.main2 + '\n' + message.main3,
+              "title": "Mains",
+              "text": message.main1 + '\n' + message.main2 + '\n' + message.main3,
+              "color": "#1a0dab"
+            }
+          ],
+    });   
+  }
+
+};
